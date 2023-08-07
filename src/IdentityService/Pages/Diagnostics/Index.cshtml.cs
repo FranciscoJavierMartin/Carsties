7732add202f97dfeb3c97b0884c1305cd8390b94
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-07 23:01:13 +0200

    Fix problem with credentials in docker

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -13,7 +13,13 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new string[] { "::ffff:172.18.0.1", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+        // I know that is weird but it is to avoid problems when using Docker
+        var localAddresses = new string[] {
+            HttpContext.Connection.RemoteIpAddress.ToString(),
+            "127.0.0.1",
+            "::1",
+            HttpContext.Connection.LocalIpAddress.ToString()
+        };
 
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
         {
