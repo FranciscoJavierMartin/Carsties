b74146d155341814727ed5bdbe80e8b32a5904b7
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-18 23:32:15 +0200

    Example authenticated request

---
+++ frontend/web-app/app/session/AuthTest.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React, { useState } from 'react';
+import { UpdateAuctionTest } from '../actions/auctionActions';
+import { Button } from 'flowbite-react';
+
+export default function AuthTest() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<any>();
+
+  function doUpdate() {
+    setResult(undefined);
+
+    setIsLoading(true);
+    UpdateAuctionTest()
+      .then((res) => setResult(res))
+      .finally(() => setIsLoading(false));
+  }
+  return (
+    <div className='flex items-center gap-4'>
+      <Button outline isProcessing={isLoading} onClick={doUpdate}>
+        Test auth
+      </Button>
+      <div>{JSON.stringify(result, null, 2)}</div>
+    </div>
+  );
+}
