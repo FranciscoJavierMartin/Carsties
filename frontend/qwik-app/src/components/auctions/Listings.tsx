bc83a290e5ec6bfb67137d8e730e86fe356fcf50
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-19 13:13:32 +0200

    Grab data

--- frontend/qwik-app/src/components/auctions/Listings.tsx
+++ frontend/qwik-app/src/components/auctions/Listings.tsx
@@ -2,19 +2,14 @@ import { Resource, component$, useResource$ } from '@builder.io/qwik';
 import { getData } from '~/server/auctions';
 
 export default component$(() => {
-  const data = useResource$(async () => {
-    await getData('');
-
-    return 'Completed';
-  });
+  const data = useResource$(async () => await getData(''));
 
   return (
     <>
-      <h1>Listings</h1>
       <Resource
         value={data}
         onPending={() => <p>Loading</p>}
-        onResolved={(data) => <p>{data}</p>}
+        onResolved={(data) => <p>{data.totalCount}</p>}
       />
     </>
   );
--- frontend/qwik-app/src/server/auctions.ts
+++ frontend/qwik-app/src/server/auctions.ts
@@ -1,5 +1,11 @@
 import { server$ } from '@builder.io/qwik-city';
 
 export const getData = server$(async function (query: string) {
-  console.log(query, this.env.get('BACKEND_URL'));
+  const res = await fetch(`${this.env.get('BACKEND_URL')}search${query}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
 });
