0d9896fbd6fdacbe7801f73caddf89bb086ef997
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-19 18:50:17 +0200

    Add icon

--- frontend/qwik-app/src/components/nav/Logo.tsx
+++ frontend/qwik-app/src/components/nav/Logo.tsx
@@ -1,8 +1,10 @@
 import { component$ } from '@builder.io/qwik';
+import { BsCarFront } from '@qwikest/icons/bootstrap';
 
 export default component$(() => {
   return (
     <div class='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
+      <BsCarFront />
       <div>Carsties Auctions</div>
     </div>
   );
