7748081dfdc008194a308d3fbef8ab7de2548903
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-20 23:21:58 +0200

    Logon use context

--- frontend/qwik-app/src/components/nav/Logo.tsx
+++ frontend/qwik-app/src/components/nav/Logo.tsx
@@ -1,9 +1,26 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext, $ } from '@builder.io/qwik';
+import { useLocation, useNavigate } from '@builder.io/qwik-city';
 import { BsCarFront } from '@qwikest/icons/bootstrap';
+import { searchContext } from '~/store/searchAuctions';
 
 export default component$(() => {
+  const reset = useContext(searchContext).reset;
+  const location = useLocation();
+  const nav = useNavigate();
+
+  const doReset = $(function (): void {
+    if (location.url.pathname !== '/') {
+      nav('/');
+    }
+
+    reset();
+  });
+
   return (
-    <div class='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
+    <div
+      onClick$={doReset}
+      class='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'
+    >
       <BsCarFront />
       <div>Carsties Auctions</div>
     </div>
