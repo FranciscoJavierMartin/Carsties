f07ee86be8eb95b2b4f744fa6912ca887996c48e
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-20 19:00:30 +0200

    order

--- frontend/qwik-app/src/components/auctions/Filters.tsx
+++ frontend/qwik-app/src/components/auctions/Filters.tsx
@@ -1,6 +1,29 @@
 import { component$ } from '@builder.io/qwik';
-import { BsStopwatchFill } from '@qwikest/icons/bootstrap';
-import { HiFireMini, HiShieldExclamationMini } from '@qwikest/icons/heroicons';
+import { BsStopwatchFill, BsStopCircleFill } from '@qwikest/icons/bootstrap';
+import {
+  HiFireMini,
+  HiShieldExclamationMini,
+  HiClockOutline,
+} from '@qwikest/icons/heroicons';
+import SortAscending from '~/components/icons/SortAscending';
+
+const orderButtons = [
+  {
+    label: 'Alphabetical',
+    icon: SortAscending,
+    value: 'make',
+  },
+  {
+    label: 'End date',
+    icon: HiClockOutline,
+    value: 'endingSoon',
+  },
+  {
+    label: 'Recently added',
+    icon: BsStopCircleFill,
+    value: 'new',
+  },
+];
 
 const filterButtons = [
   {
@@ -22,12 +45,50 @@ const filterButtons = [
 
 export default component$(() => {
   const filterBy = 'live';
+  const orderBy = 'make';
+
   return (
     <div class='flex justify-between items-center mb-4'>
       <div>
         <span class='uppercase text-sm text-gray-500 mr-2'>Filter by</span>
         <div class='inline-flex rounded-md shadow-sm' role='group'>
           {filterButtons.map(({ label, icon: Icon, value }, index: number) => (
+            <button
+              key={value}
+              class={[
+                'inline-flex',
+                'items-center',
+                'px-4',
+                'py-2',
+                'text-sm',
+                'font-medium',
+                'text-gray-900',
+                'bg-white',
+                'border-gray-200',
+                'hover:bg-gray-100',
+                'focus:z-10',
+                'focus:ring-2',
+                'first-of-type:rounded-l-lg',
+                'last-of-type:rounded-r-md',
+                index === 0 || index === filterButtons.length - 1
+                  ? 'border'
+                  : 'border-t border-b',
+                {
+                  'text-red-900 border-red-300 enabled:hover:bg-red-100 focus:ring-4 focus:ring-red-300 :bg-red-600 focus:text-red-900':
+                    filterBy === value,
+                },
+              ]}
+            >
+              <Icon class='mr-3 h-4 w-4' />
+              {label}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div>
+        <span class='uppercase text-sm text-gray-500 mr-2'>Order by</span>
+        <div class='inline-flex rounded-md shadow-sm' role='group'>
+          {orderButtons.map(({ label, icon: Icon, value }, index: number) => (
             <button
               key={value}
               class={[
@@ -51,7 +112,7 @@ export default component$(() => {
                   : 'border-t border-b',
                 {
                   'text-red-900 border-red-300 enabled:hover:bg-red-100 focus:ring-4 focus:ring-red-300 :bg-red-600 focus:text-red-900':
-                    filterBy === value,
+                    orderBy === value,
                 },
               ]}
             >
