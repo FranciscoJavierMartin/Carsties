d89822e74909dbbe50e1c45751ab7366aa3256e9
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-29 19:11:51 +0200

    Create form

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Heading from '@/app/components/Heading';
+import AuctionForm from '@/app/auctions/AuctionForm';
 
 export default function Create() {
   return (
-    <div>page</div>
-  )
+    <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
+      <Heading
+        title='Sell your car!'
+        subtitle='Please enter the details of your car'
+      />
+      <AuctionForm />
+    </div>
+  );
 }
