6fd06efccd0ecbc20b8425774bd7653b763424da
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-30 23:12:12 +0200

    Auction detailed page

--- frontend/web-app/app/providers/ToasterProvider.tsx
+++ frontend/web-app/app/providers/ToasterProvider.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function ToasterProvider() {
-  return <div>ToasterProvider</div>;
+  return <Toaster position='bottom-right' />;
 }
